80e45e94a2eadbba267116ec69d1a01fc452a8db
MD Shamiul Islam <77440860+Shamiul-693@users.noreply.github.com>
2025-03-04 18:49:56 +0600

    setup.py

--- sami/setup.py
+++ sami/setup.py
@@ -7,9 +7,11 @@ setup(
     install_requires=[
         "matplotlib",
         "seaborn",
+        "numpy",
+        "pandas"
     ],
-    author="Sami",
-    description="A simple visualization package",
+    author="Md Shamiul Islam",
+    description="A simple Python visualization package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
 )
